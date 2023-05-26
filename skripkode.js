1724340cb0b195b4289369ea16092a94b8effae9
Niko Fendi Lienata <37220554+nikofendi@users.noreply.github.com>
2023-05-27 00:56:55 +0700

    ganti js code

--- skripkode.js
+++ skripkode.js
@@ -1,11 +1,11 @@
 <script>
-	 /*  Toggle between showing and hiding the   nav     igation menu links when the user clicks on the      hamburger menu icon */
-        function myFunction() {
-	 		var x = document.getElementById("myNavbar");
-        if (x.className === "navbar") {
-                x.className += " responsive";
-			} else {
-                x.className = "navbar";
-			}
+	function myFunction() {
+		var x = document.getElementById("myNavbar");
+		if (x.className === "navbar") {
+			x.className += " responsive";
+		} else {
+			x.className = "navbar";
 		}
+	}
 </script>
+
