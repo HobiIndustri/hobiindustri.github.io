c9d9e3baabb9cd3d6e585b38439c5d9cdb828e8c
Niko Fendi Lienata <37220554+nikofendi@users.noreply.github.com>
2023-05-27 01:07:21 +0700

    ganti coba js

--- skripkode.js
+++ skripkode.js
@@ -1,11 +1,10 @@
 <script>
 function myFunction() {
     var x = document.getElementById("myNavbar");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
+    if (x.className === "navbar") 
+    {x.className += "responsive"} 
+    else 
+    {x.className = "navbar"}
 }
 </script>
 
